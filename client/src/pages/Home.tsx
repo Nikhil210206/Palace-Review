ad52b7ecbdce9894097055aa9b38d053503bc7c2
Nikhil <nikhilbalamurugan@gmail.com>
2025-03-21 22:08:03 +0530

    added 7th button

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -202,8 +202,8 @@ export default function Home() {
       <Footer onNavigate={scrollToTop} />
 
       {/* Audio Player and Buttons */}
-      <div className="fixed bottom-6 right-6 flex flex-col space-y-2">
-        {Array.from({ length: 6 }, (_, i) => (
+      <div className="fixed bottom-7 right-7 flex flex-col space-y-2">
+        {Array.from({ length: 7 }, (_, i) => (
           <div key={i} className="flex items-center">
             <button
               onClick={() => toggleAudio(`audioPlayer${i + 1}`)}
